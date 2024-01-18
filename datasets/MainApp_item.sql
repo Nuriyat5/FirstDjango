a657d694b7906225041dab6b2fa50fd8368e54fe
Nuriyat Abdulagatova <nabdulagatova@mail.ru>
2024-01-18 23:29:14 +0300

    Create MainApp_item.sql

---
+++ datasets/MainApp_item.sql
@@ -0,0 +1,6 @@
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (1,'Кроссовки','abibas',10,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (2,'Куртка кожаная','Nike',2,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (3,'Coca-cola 1 литр','Coca-cola',12,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (4,'Картофель фри','точка',0,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (5,'Кепка','ООО Текстиль',124,'Базовое описание');
+INSERT INTO "MainApp_item" ("id", "name", "brand", "count", "description") VALUES (6,'Компьютер','IBM',10,'IBM-PC 286');
